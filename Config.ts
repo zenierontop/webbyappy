616a947b49aad71723eb7c6fa2724d8fbf536536
zenierontop <159908629+zenierontop@users.noreply.github.com>
2024-02-16 15:05:45 +0200

    Update Config.ts

--- Config.ts
+++ Config.ts
@@ -1,14 +1,6 @@
 export const Config = {
     ChatSound: "//computernewb.com/collab-vm/notify.ogg",
     ServerAddresses: [
-        "wss://computernewb.com/collab-vm/vm0",
-        "wss://computernewb.com/collab-vm/vm1",
-        "wss://computernewb.com/collab-vm/vm2",
-        "wss://computernewb.com/collab-vm/vm3",
-        "wss://computernewb.com/collab-vm/vm4",
-        "wss://computernewb.com/collab-vm/vm5",
-        "wss://computernewb.com/collab-vm/vm6",
-        "wss://computernewb.com/collab-vm/vm7",
-        "wss://computernewb.com/collab-vm/vm8",
+        "wss://psh3kf-6004.csb.app",
     ]
 }
